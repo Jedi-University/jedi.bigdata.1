0bd83e5d06e64b81ebe750aefcd226d07f695952
Aleksei Zavarzin <alex.zavr.27100@mail.ru>
2021-10-15 22:16:56 +0400

    Update homework-context.py (task 3)

--- azavarzin/homework-context.py
+++ azavarzin/homework-context.py
@@ -15,10 +15,15 @@ def func(a, b, c):
 # Необходимо написать код который превратит его в простой лист
 # [1,2,3,4,5,6,0,0,0,0,"a", "b", "c"]
 # для решения данной задачи нельзя использовать доп массива
-
-# def task_2(l):
-#     return reduce(lambda a, b: a + b, l)
 join_list_of_lists_into_list = lambda l: reduce(lambda a, b: a + b, l)
+
+# 3. написать реализацию метода caller
+# результат: "100h - 200z"
+def a(x,y):
+    print(f"{x} - {y}")
+    
+def caller(func, c, j):
+    return lambda a, b: func(c + a, j + b)
 
 
 if __name__ == "__main__":
@@ -29,3 +34,7 @@ if __name__ == "__main__":
     # task 2
     l = [[1, 2, 3, 4, 5, 6], [0, 0, 0, 0], ["a", "b", "c"]]
     print(join_list_of_lists_into_list(l))
+    
+    # task 3
+    f = caller(a, "100", "200")
+    f("h", "z")
